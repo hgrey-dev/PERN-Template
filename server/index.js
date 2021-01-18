018a840177fb8d5e5047f71e329cc00a2f3d0368
hgrey-dev <67564128+hgrey-dev@users.noreply.github.com>
2021-01-18 15:16:44 +1300

    nodemon added

--- server/index.js
+++ server/index.js
@@ -10,3 +10,19 @@ app.use(express.json());
 app.listen(5000,()=> {
     console.log("server is started on port 5000")
 });
+
+//ROUTES//
+
+//create a new player
+
+app.post("/player", async (req,res) => {
+    try {
+        console.log(req.body);
+        // const { firstName, lastName }
+        // const newPlayer = await pool.query("INSERT INTO leader_board VALUES ($1)($2)"[firstName,lastName]);
+        // res.json(newPlayer)
+    } catch (err) {
+        console.error(err.message);///not owrking at the moment..test with insomnia
+    }
+    
+})
